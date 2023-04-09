8dd839d0d6ee66a11a439fd76e4212387ac666fa
maochwei <773848936@qq.com>
2023-04-09 21:02:25 +0800

    add music 1

--- blog.config.js
+++ blog.config.js
@@ -116,10 +116,10 @@ const BLOG = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [ // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
+      name: 'a new day',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
-      cover: 'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+      url: 'https://music.163.com/song/media/outer/url?id=28302848.mp3',
+      cover: 'http://p2.music.126.net/k6jxqMX4gHH1vkaUlt1lAQ==/3432675305537587.jpg'
     },
     {
       name: '王都グランセル',
