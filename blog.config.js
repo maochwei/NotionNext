f925de8f6ad2a79f0a2aae33f0fd2a9829eba0b6
maochwei <773848936@qq.com>
2023-04-09 21:11:54 +0800

    add muisc aaa

--- blog.config.js
+++ blog.config.js
@@ -122,10 +122,10 @@ const BLOG = {
       cover: 'http://p2.music.126.net/k6jxqMX4gHH1vkaUlt1lAQ==/3432675305537587.jpg'
     },
     {
-      name: '王都グランセル',
+      name: 'you & i',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
-      cover: 'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+      url: 'https://music.163.com/song/media/outer/url?id=29769611.mp3',
+      cover: 'http://p1.music.126.net/my8jpMEwcj5lH_KegnN2Fw==/109951163679445839.jpg'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
