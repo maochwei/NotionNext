9e55dc8ed91bf330794d4131303089a0c3a6c152
maochwei <773848936@qq.com>
2023-04-16 22:10:34 +0800

    baidu-site

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -18,6 +18,7 @@ const CommonHead = ({ meta, children }) => {
   return (
     <Head>
       <title>{title}</title>
+      <meta name="baidu-site-verification" content="codeva-p2k0dwRwDY" />
       <meta name="theme-color" content={BLOG.BACKGROUND_DARK} />
       <meta
         name="viewport"
